ea0d49e81206234dce4a88573a5bb2a081c2672f
derek williams <derek61@gmail.com>
2025-01-10 16:33:29 -0700

    rolled back to version 0.6.0 of the swc compiler

--- README.md
+++ README.md
@@ -31,3 +31,9 @@ bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+### SWC RS Compiler Example
+
+see https://github.com/Dwlad90/stylex-swc-plugin/tree/develop/apps/nextjs-example
+
+Note: as of time of this writing the example is using version 0.6.1-rc.1.  Update this once a non-rc release is available.
--- next.config.js
+++ next.config.js
@@ -9,6 +9,23 @@
 
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+const path = require('path');
+const stylexPlugin = require('@stylexswc/nextjs-plugin');
+const rootDir = __dirname;
+
+module.exports = stylexPlugin({
+  // Add any StyleX options here
+  rsOptions: {
+    aliases: {
+      '@/*': [
+        path.join(rootDir, '*'),
+      ],
+    },
+    unstable_moduleResolution: {
+      type: 'commonJS',
+    },
+  }
+})({
   transpilePackages: ['@stylexjs/open-props'],
-};
+  // Optionally, add any other Next.js config below, e.g. 
+});
