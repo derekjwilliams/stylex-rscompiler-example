fcb2cc003d67428edb05a3c3161f0f6ac674c60c
derek williams <derek61@gmail.com>
2025-01-10 15:09:43 -0700

    update packages

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # stylex-rscompiler-example
+
+## Source
+
+This example is based on https://github.com/facebook/stylex/tree/main/apps/nextjs-example
+
+## Getting Started
+
+### Install 
+
+```bash
+npm install
+```
+
+### Build (optional)
+
+```bash
+npm build
+```
+
+### Run Dev
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ babel.config.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ */
+const path = require('path');
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    ['@babel/plugin-transform-private-methods'],
+    [
+      '@stylexjs/babel-plugin',
+      // See all options in the babel plugin configuration docs:
+      // https://stylexjs.com/docs/api/configuration/babel-plugin/
+      {
+        dev: process.env.NODE_ENV === 'development',
+        runtimeInjection: false,
+        genConditionalClasses: true,
+        treeshakeCompensation: true,
+        aliases: {
+          '@/*': [path.join(__dirname, '*')],
+        },
+        unstable_moduleResolution: {
+          type: 'commonJS',
+        },
+      },
+    ],
+  ],
+};
+
+// const path = require('path')
+// module.exports = {
+//   presets: ['next/babel'],
+//   plugins: [
+//     ['@babel/plugin-transform-private-methods'],
+//     [
+//       '@stylexjs/babel-plugin',
+//       {
+//         dev: process.env.NODE_ENV === 'development',
+//         runtimeInjection: false,
+//         genConditionalClasses: true,
+//         treeshakeCompensation: true,
+//         unstable_moduleResolution: {
+//           type: 'commonJS',
+//           rootDir: __dirname,
+//         },
+//       },
+//     ],
+//   ],
+//   env: {
+//     development: {
+//       compact: false,
+//     },
+//   },
+// }
+
